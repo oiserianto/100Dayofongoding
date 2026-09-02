46fc5204e81dabb77ba31a2977b4272603236742
oiserianto <oiserianto20@gmail.com>
2026-09-02 20:43:35 +0800

    Create Day1.java
    File latihan day 1

---
+++ Day1.java
@@ -0,0 +1,35 @@
+Java tidak menjalankan tulisan setelah //.
+// Ini adalah komentar satu baris
+
+Untuk komentar lebih dari satu baris menggunakan tanda /*serta ada penutup/*
+/*
+ * Ini adalah komentar
+ * yang terdiri dari beberapa baris
+ */
+
+ini Biasanya digunakan untuk dokumentasi class atau method menggunakan tanda /** dan penutupnya hanya 1 bintang/*
+/**
+ * Ini adalah dokumentasi untuk class.
+ * disini bisa kita bisa untuk menjelaskan kode.
+ */
+
+
+// Ini adalah class. Nama class-nya codingan kita adalah StrukturJava.
+public class StrukturJava { 
+
+  
+// Ini adalah method utama (main). Program Java mulai dijalankan dari sini.
+    public static void main(String[] args) {
+
+        // ini digunakan untuk menampilkan tulisan ke layar.
+        System.out.println();
+
+        // Membuat variabel nama bertipe String.
+        String nama =
+        // Membuat variabel umur bertipe int (bilangan bulat).
+        int umur =
+
+        // Menampilkan isi variabel
+        System.
+    }
+}
