f160ac742f4b01b24b2ad05d14d416c8fd417b8a
oiserianto <oiserianto20@gmail.com>
2026-09-11 06:12:35 +0800

    Implement program to show numeric data type limits
    Latihan java Day10

---
+++ Day10.java
@@ -0,0 +1,43 @@
+//Day10 Pemahaman batas Minimal dan maksimal dari tipe data
+ 
+/**
+ * Program untuk melihat batas minimum
+ *  dan maksimum tipe data numerik java 
+ */
+public class Day10 {
+    public static void main(String[] args) {
+        
+        System.out.println("=========== PORGRAM JAVA DAY 10 ==================");
+
+
+        // byte
+        //menampilkan nilai minimum byte.
+        System.out.println("BYTE MIN    : "+Byte.MIN_VALUE);
+
+        // Menampilkan nilai maksimun byte.
+        System.out.println("BYTE MAX    : "+Byte.MAX_VALUE);
+
+        // short
+        //menampilkan nilai minimum short.
+        System.out.println("SHORT MIN   : "+Short.MIN_VALUE);
+
+        //Menampilkan nilai maksimum short.
+        System.out.println("SHORT MAX   : "+Short.MAX_VALUE);
+
+        // int
+        //menampilkan nilai minimum int
+        System.out.println("INT MIN     : "+Integer.MIN_VALUE);
+
+        //menampilkan nilai maksimum int
+        System.out.println("INT MAX     : "+Integer.MAX_VALUE);
+
+        //long
+        //menampilakn nilai minimum long
+        System.out.println("LONG MIN    : "+Long.MIN_VALUE);
+
+        //menampilkan nilai maxsimun lonf
+        System.out.println("LONG MAX    : "+Long.MAX_VALUE);
+
+        System.out.println("============ SAMPAI JUMPA DI DAY 11 ==============");
+    }
+}
