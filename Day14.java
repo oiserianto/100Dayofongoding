7431060a83705a4e0c95c8459bd2e7482ee62964
oiserianto <oiserianto20@gmail.com>
2026-09-15 17:41:25 +0800

    Implement arithmetic operations for addition and subtraction
    Latihan java Day14

---
+++ Day14.java
@@ -0,0 +1,24 @@
+//Day14 operator aritmatika penjumlahan(+)dan pengurangan(-)
+
+public class Day14 {
+    public static void main(String[] args) {
+        
+        //menyimpan angka 1
+        int angka1 = 50;
+
+        //menyimpan angka 2
+        int angka2 = 5;
+
+        //(penjumlahan) digunakan untuk menjumlahkan  dua nilai atau lebih
+        //menghitung penjumlahan
+        int hasiljumlah = angka1+angka2;
+
+        //(pengurangan) digunakan untuk mengurangi suatu nilai dengan nilai lainnya
+        //menghitung pengurangan
+        int hasilkurang = angka1-angka2;
+
+        // menampilkan hasil penjumlahan
+        System.out.println("Penjumlahan : "+hasiljumlah);
+        System.out.println("pengurangan : "+hasilkurang);
+    }
+}
