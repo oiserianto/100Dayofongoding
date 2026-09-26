5ac03009093c1d1494c99cc2ae416107bd7b6a24
oiserianto <oiserianto20@gmail.com>
2026-09-27 06:11:20 +0800

    Add Day26.java for increment and decrement example
    Latihan java Day26

---
+++ Day26.java
@@ -0,0 +1,12 @@
+// Day26 Operator Increment dan Decrement (++,--)
+public class Day26 {
+    public static void main(String[] args) {
+       int angka = 10;
+       System.out.println("Nilai awal\t\t: "+angka);
+       angka++;
+       System.out.println("Sebelum increment\t: "+angka);
+       angka--;
+       System.out.println("Setelah decrement\t: "+angka);
+        
+    }
+}
